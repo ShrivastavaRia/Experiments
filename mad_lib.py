45cd35bf9311a6c5280dd85a9f34f5144a22ddbf
ShrivastavaRia <45320906+ShrivastavaRia@users.noreply.github.com>
2020-06-18 21:06:34 +0200

    mad lib generator for funky stories

---
+++ mad_lib.py
@@ -0,0 +1,12 @@
+
+colour=input("enter the color:")
+adjective=input("enter the adjective:")
+food=input("enter the food:")
+
+
+print("roses are "+ colour)
+print(adjective + " are blue ")
+print("i love " + food)
+
+    
+
